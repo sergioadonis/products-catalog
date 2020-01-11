ae7ac6a7c56da1d5d0d0ff5bd678c70f173c4d64
Sergio Adonis <sergio.adonis@hotmail.com>
2020-01-11 16:12:57 -0600

    basic-schema function, removing innecesary imports

--- src/basic-schema.js
+++ src/basic-schema.js
@@ -1,12 +1,5 @@
 /* eslint-disable object-curly-newline */
-const {
-  CATEGORIES,
-  ID,
-  NAME,
-  DESCRIPTION,
-  PHOTO_URL,
-  PERSONS,
-} = require('./constants');
+const { ID, NAME, DESCRIPTION, PHOTO_URL } = require('./constants');
 
 const basicSchema = table => {
   table.increments(ID).primary();
